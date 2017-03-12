b668c5dea7341521fdc7174d0dc53416f189263f
romajs <joseromualdoneto@gmail.com>
2017-03-12 19:06:27 -0300

    express winston logger

--- src/app.js
+++ src/app.js
@@ -1,7 +1,30 @@
 var express = require('express')
+var expressWinston = require('express-winston')
 var path = require('path')
+var winston = require('winston')
+
+var config = {
+  logger : {
+    transports: [
+      new winston.transports.Console({
+        colorize: true,
+        timestamp: true,
+      })
+    ],
+    level : 'info',
+    exitOnError: false,
+    expressFormat: true,
+    colorize: true,
+  },
+	server : {
+		port : 8000,
+	}
+}
 
 var app = express()
+
+// logger
+app.use(expressWinston.logger(config.logger));
 
 // resources
 app.use('/ad', require('./api/resource/ad.js'))
@@ -10,9 +33,19 @@ app.use('/user', require('./api/resource/user.js'))
 // static
 app.use(express.static(path.join(__dirname, 'web')))
 
+// error logger
+app.use(function (err, req, res, next) {
+  res.status(500).send({
+    status: 500,
+    message: 'internal error',
+    type: 'internal',
+  })
+  next(err)
+})
+
 // server
-server = app.listen(8000, function () {
-  console.info('App listening on:', server.address())
+server = app.listen(config.server.port, function () {
+  new (winston.Logger)(config.logger).info('App listening on:', server.address())
 })
 
 module.exports = app

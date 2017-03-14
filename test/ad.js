959a007b8582ec3f992ab41310a4b7d86f8e8b77
romajs <joseromualdoneto@gmail.com>
2017-03-13 22:40:40 -0300

    unit testing: express and mocha without child_process/fork (1s -> 170ms)

--- test/ad.js
+++ test/ad.js
@@ -2,34 +2,30 @@ var app = require('../src/app.js')
 var assert = require('assert')
 var mockCouch = require('mock-couch')
 var request = require('supertest')
-var fork = require("child_process").fork
 
 describe('/ad', function() {
 
+	var server = null, couchdb = null
+
 	beforeEach(function(done) {
 
-    child = fork('./src/server.js', null, {});
+		couchdb = mockCouch.createServer()
 
-    child.on('message', function (msg) {
-      if (msg === 'listening') {
-      	couchdb = mockCouch.createServer()
-		    couchdb.listen(5984, function() {
-		    	require('nano')('http://localhost:5984').db.create('ad')
-		    	done()
-		    })
-      }
-    });
+		couchdb.listen(5984, function() {
+			require('nano')('http://localhost:5984').db.create('ad')
+			server = app.listen(8000, done)
+		})
 
 	})
  
 	afterEach(function(done) {
-		child.kill();
-		couchdb.close(done)
+		couchdb.close(function() {
+			server.close(done)
+		})
 	})
 
 	it('get', function(done) {
-		request(app)
-			.get('/ad')
+		request(app).get('/ad')
 			.expect(function(res) {
 				assert(res.body.offset == 0)
 				assert(res.body.total_rows == 0)
@@ -39,8 +35,7 @@ describe('/ad', function() {
 	})
  
 	it('post', function(done) {
-		request(app)
-			.post('/ad')
+		request(app).post('/ad')
 			.expect(function(res) {
 				assert(res.body.ok == true)
 				assert(res.body.id != null)

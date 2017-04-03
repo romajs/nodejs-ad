c6a46e61665d5f33205b5f18283ba984949bea40
romajs <joseromualdoneto@gmail.com>
2017-04-03 00:01:48 -0300

    removed commentec code

--- test/fixture/01-user.js
+++ test/fixture/01-user.js
@@ -1,22 +1,5 @@
 module.exports = function(config, callback) {
 
-	// var nano = require('nano')(config.couchdb.url())
-
-	// nano.db.use('user').insert({
-	// 	username : 'admin',
-	// 	password : 'MTIzbXVkYXIK',
-	// 	admin : true,
-	// }, function(err, body) {
-	// })
-
-	// nano.db.use('user').insert({
-	// 	username : 'user',
-	// 	password : 'MTIzbXVkYXIK',
-	// 	admin : false,
-	// }, function(err, body) {
-	// 	callback && callback()
-	// })
-
 	var Datastore = require('@google-cloud/datastore')
 
 	datastore = Datastore({

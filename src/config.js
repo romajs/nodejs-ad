3e86c5b96c0cb9c924f9ed2db952b20d66e3af20
romajs <joseromualdoneto@gmail.com>
2017-04-07 19:36:11 -0300

    config env factory

--- src/config.js
+++ src/config.js
@@ -1,36 +1,51 @@
 var winston = require('winston')
 
-var config = {
-	auth : {
-		header_name : 'x-access-token',
-		secret : 'JHVwM3JfJDNjcjM3Cg==',
-		expiresIn : 86400 // expires in 24 hours
-	},
-	logger : {
-		transports: [
-			new winston.transports.Console({
-				colorize: true,
-				timestamp: true
-			})
-		],
-		level : 'info',
-		exitOnError: false,
-		expressFormat: true,
-		colorize: true
-	},
-	mongodb : {
-		protocol : 'mongodb',
-		host : process.env.MONGODB_HOST || 'localhost',
-		port : process.env.MONGODB_PORT || 27017,
-		dbname : 'nodejs-ad',
-		url : function() {
-			return [this.protocol, '://', this.host, ':', this.port, '/', this.dbname].join('')
+process.env.NODE_ENV = 'dev'
+
+function config(env) {
+
+	var baseConfig = {
+		auth : {
+			header_name : 'x-access-token',
+			secret : 'JHVwM3JfJDNjcjM3Cg==',
+			expiresIn : 86400 // expires in 24 hours
+		},
+		logger : {
+			transports: [
+				new winston.transports.Console({
+					colorize: true,
+					timestamp: true
+				})
+			],
+			level : 'info',
+			exitOnError: false,
+			expressFormat: true,
+			colorize: true
+		},
+		mongodb : {
+			protocol : 'mongodb',
+			host : process.env.MONGODB_HOST || 'localhost',
+			port : process.env.MONGODB_PORT || 27017,
+			dbname : 'nodejs-ad',
+			url : function() {
+				return [this.protocol, '://', this.host, ':', this.port, '/', this.dbname].join('')
+			}
+		},
+		http : {
+			host : process.env.HTTP_HOST || '127.0.0.1',
+			port : process.env.HTTP_PORT || 8000
 		}
-	},
-	http : {
-		host : process.env.HTTP_HOST || '127.0.0.1',
-		port : process.env.HTTP_PORT || 8000
 	}
+
+	var configs = {}
+
+	configs.dev = Object.assign(baseConfig)
+	configs.dev.mongodb.host = 'localhost'
+	configs.dev.mongodb.port = 27017
+	configs.dev.mongodb.dbname = 'nodejs-ad'
+
+	return configs[env]
+
 }
 
-module.exports = config
+module.exports = config(process.env.NODE_ENV)

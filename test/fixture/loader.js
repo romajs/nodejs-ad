8f682fec61c6df75434ddac020f59da2c9121822
romajs <joseromualdoneto@gmail.com>
2017-10-22 19:07:13 -0200

    fix fixture loader

--- test/fixture/loader.js
+++ test/fixture/loader.js
@@ -5,14 +5,17 @@ var App = require('../../src/app')
 var fixture = process.argv[2]
 
 if (!fixture) {
-  process.stdout.write('fixture is required for loading.')
-  process.stdout.write('usage: node test/fixture/loader test/fixture/xx-script.js')
+  console.log('fixture is required for loading.')
+  console.log('usage: node test/fixture/loader test/fixture/xx-script.js')
   process.exit(1)
 }
 
-process.stdout.write('loading fixture: "%s"', fixture)
+console.log('loading fixture: "%s"', fixture)
 
 fixture = require(process.cwd() + '/' + fixture)
-fixture.load()
 
-App.db.close()
+fixture.load().then(function() {
+    App.db.close()
+}, function(err) {
+    console.error(err)
+})
